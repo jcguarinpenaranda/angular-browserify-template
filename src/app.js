97f579d87157df6625947425c0477bb9bc53f084
Juan Camilo Guarín Peñaranda <jcguarinpenaranda@gmail.com>
2016-08-17 20:47:57 -0500

    initial upload

---
+++ src/app.js
@@ -0,0 +1,11 @@
+var angular = require('angular');
+var uiRouter = require('angular-ui-router');
+
+
+angular.module('app', ['ui.router'])
+.config(['$stateProvider', function(){
+    
+}])
+.run(function(){
+    console.log('running')
+})
---
+++ src/pages/home/index.js
@@ -0,0 +1,14 @@
+var angular = require('angular');
+var moduleName = "";
+var controllerName = "";
+
+angular.module(moduleName, [])
+.config(['$stateProvider', function($stateProvider){
+
+}])
+
+.controller(controllerName, [function(){
+  
+}])
+
+module.exports = moduleName;
